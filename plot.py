5bdd39091592557f4e54945cca432baa4b54c9c6
Guangli Hu <3409961762@qq.com>
2025-03-28 17:58:14 +0800

    Update plot.py

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ from torchvision.datasets import FashionMNIST
 from torchvision import transforms
 import numpy as np
 import torch.utils.data as Data
-train_data=FashionMNIST(root='./人工智能/神经网络/LeNet-5/data',
+train_data=FashionMNIST(root='./data',
                         train=True,
                         transform=transforms.Compose([transforms.Resize(size=224),transforms.ToTensor()]),
                         download=True)
